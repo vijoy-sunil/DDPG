37120025f3b3762dfe96b9cc667cd96758936b1c
vijoy-sunil <vijoy.sunil@gmail.com>
2022-02-13 10:49:43 +0530

    deque as replay buffer

--- ReplayBuffer.py
+++ ReplayBuffer.py
@@ -1,50 +1,32 @@
 import numpy as np
-import tensorflow as tf
+from collections import deque
 
 class ReplayBuffer:
     def __init__(self, capacity, batch_size, state_space, action_space):
         self.buffer_capacity = capacity
-        self.buffer_counter = 0
         # sampling batch size
         self.batch_size = batch_size
-        # buffer must store (state, action, reward, next_state)
-        self.state_buffer = np.zeros((self.buffer_capacity, state_space))
-        self.action_buffer = np.zeros((self.buffer_capacity, action_space))
-        self.reward_buffer = np.zeros((self.buffer_capacity, 1))
-        self.next_state_buffer = np.zeros((self.buffer_capacity, state_space))
-        self.done_buffer = np.zeros((self.buffer_capacity, 1))
+        # buffer
+        self.buffer = deque(maxlen=self.buffer_capacity)
 
     def get_size(self):
-        return self.buffer_counter
+        return len(self.buffer)
 
     # left most element gets popped when size exceeds maxlen
     def add_experience(self, experience):
         state, action, reward, next_state, done = experience
-        # Set index to zero if buffer_capacity is exceeded, replacing
-        # old records
-        index = self.buffer_counter % self.buffer_capacity
-        self.state_buffer[index] = state
-        self.action_buffer[index] = action
-        self.reward_buffer[index] = reward
-        self.next_state_buffer[index] = next_state
-        self.done_buffer[index] = done
-        # increment counter
-        self.buffer_counter += 1
+        # add to deque
+        self.buffer.append([state,
+                            action,
+                            np.expand_dims(reward, -1),
+                            next_state,
+                            np.expand_dims(done, -1)])
 
     def sample_batch(self):
-        record_range = min(self.buffer_counter, self.buffer_capacity)
-        batch_indices = np.random.choice(record_range, self.batch_size)
-        # Convert to tensors
-        state_batch = tf.convert_to_tensor(self.state_buffer[batch_indices])
-        action_batch = tf.convert_to_tensor(self.action_buffer[batch_indices])
-        reward_batch = tf.convert_to_tensor(self.reward_buffer[batch_indices])
-        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
-        next_state_batch = tf.convert_to_tensor(self.next_state_buffer[batch_indices])
-
-        done_batch = tf.convert_to_tensor(self.done_buffer[batch_indices])
-        done_batch = tf.cast(done_batch, dtype=tf.float32)
-
-        return state_batch, action_batch, reward_batch, next_state_batch, done_batch
-
+        indices = np.random.choice(len(self.buffer),
+                                   size=min(self.batch_size, len(self.buffer)),
+                                   replace=False)
+        batch = [self.buffer[index] for index in indices]
+        return batch
 
 
